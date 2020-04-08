b29ef58007e391b395cf79d0d4a44bb19e64d637
bblika <63354304+bblika@users.noreply.github.com>
2020-04-08 21:47:33 +0500

    Add files via upload

---
+++ sssss/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ssssss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fstream = File.OpenRead($"D:\\bb.txt"))
+            {
+
+                byte[] array = new byte[fstream.Length];
+
+                fstream.Read(array, 0, array.Length);
+
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+
+                Console.WriteLine(Regex.Replace(textFromFile,
+                @"(?<month>\d{1,2})/(?<day>\d{1,2})/(?<year>\d{2,4})",
+                "${day}.${month}.${year}"));
+            }
+        }
+    }
+}
